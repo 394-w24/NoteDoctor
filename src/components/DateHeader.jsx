bbf4a734719de396eea82b24d441365a22466631
kemei-8 <84368186+kemei-8@users.noreply.github.com>
2024-02-16 14:07:24 -0600

    added close button

--- src/components/DateHeader.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { formatDate } from "../utils/dates";
-
-export const DateHeader = () => {
-  const today = formatDate(new Date());
-  return <div className="fixed right-0 top-0">{today}</div>;
-};
--- src/utils/dates.js
+++
@@ -1,5 +0,0 @@
-export const formatDate = (date) => {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  const formattedDate = date.toLocaleDateString('en-US', options);
-  return formattedDate;
-};

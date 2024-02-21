40f8dbff026ed9801c73bb8ca51578627af24b1d
Rodney Reichert <rodneyreichert2024@u.northwestern.edu>
2024-02-20 19:00:49 -0600

    added query to firebase.js

--- src/pages/CheckIn.jsx
+++ src/pages/CheckIn.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import {getPatient, getAppt, getCareGiver} from "../utils/firebase"
+import { useQuery } from 'react-query';
 
 const CheckIn = () => {
+  const patData = getPatient("2ScZbzYKjN7leSH2V6ro");
+  getAppt("KMa3tIVTBbLKpv9etNau");
+  getCareGiver("a8be0g5CGUvm3ARLAxDb")
   return (
     <main>
-      <h1 className="font-cursive text-5xl">Carly Lowell (She/Her)</h1>
+      <h1 className="font-cursive text-5xl">{patData.firstName} {patData.lastName} ({patData.pronouns})</h1>
     </main>
   );
 };
---
+++ src/utils/firebase.js
@@ -0,0 +1,62 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsVo2H8mKsXh6iiuZtoo7Hnz1Xigp0ScY",
+  authDomain: "notedoctor-d96e7.firebaseapp.com",
+  projectId: "notedoctor-d96e7",
+  storageBucket: "notedoctor-d96e7.appspot.com",
+  messagingSenderId: "1053079986503",
+  appId: "1:1053079986503:web:8bc20717f73a9288fdec62",
+  measurementId: "G-E2E401FCH5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export const getPatient = async (uuid) => {
+  const docRef = doc(db, "patients", uuid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+    return docSnap.data()
+  } else {
+    // docSnap.data() will be undefined in this case
+    console.log("No such document!");
+  }
+
+}
+
+export const getAppt = async (uuid) => {
+  const docRef = doc(db, "appointments", uuid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+    return docSnap.data()
+  } else {
+    // docSnap.data() will be undefined in this case
+    console.log("No such document!");
+  }
+
+}
+
+export const getCareGiver = async (uuid) => {
+  const docRef = doc(db, "caregivers", uuid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+    return docSnap.data()
+  } else {
+    // docSnap.data() will be undefined in this case
+    console.log("No such document!");
+  }
+}
+
+export default getPatient

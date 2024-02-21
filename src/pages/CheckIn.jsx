669a3ddd60101a0f48ea040bb36b44104bc1a3e1
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-02-20 21:49:41 -0600

    Styling

--- src/pages/CheckIn.jsx
+++ src/pages/CheckIn.jsx
@@ -12,55 +12,66 @@ const CheckIn = () => {
   if (isLoading) return "Loading...";
 
   return (
-    <main className="mx-auto my-4 max-w-md">
-      <h1 className="font-cursive text-5xl">
-        {apptData.patient.firstName} {apptData.patient.lastName} (
-        {apptData.patient.pronouns})
-      </h1>
-
-      <div className="flex justify-between">
-        <div className="flex flex-col">
-          <p>{apptData.patient.dob.toDate().toLocaleDateString()}</p>
-          <p>{apptData.patient.age}</p>
+    <main className=" min-h-svh bg-gradient-to-tr from-red-400 to-sky-600">
+      <div className="mx-auto max-w-lg space-y-6 pt-8">
+        <h1 className="font-cursive text-5xl">
+          {apptData.patient.firstName} {apptData.patient.lastName} (
+          {apptData.patient.pronouns})
+        </h1>
+        <div className="flex justify-between">
+          <div className="flex flex-col">
+            <p>
+              <span className="font-semibold">Date of Birth: </span>
+              {apptData.patient.dob.toDate().toLocaleDateString()}
+            </p>
+            <p>
+              <span className="font-semibold">Age: </span>
+              {apptData.patient.age}
+            </p>
+          </div>
+          <img
+            src="/nurse.webp"
+            alt="Maria Alvarez, RN"
+            className="aspect-square h-32 w-min rounded-full"
+          />
         </div>
-
-        <img
-          src="/nurse.webp"
-          alt="Maria Alvarez, RN"
-          className="aspect-square h-32 w-min rounded-full"
-        />
-      </div>
-
-      <form className="w-full space-y-2">
-        <FormInput label="Height" name="height" value={apptData.height}/>
-        <FormInput label="Weight" name="weight" value={apptData.weight}/>
-        <FormInput label="Respiration Rate" name="respRate" value={apptData.respRate}/>
-        <FormInput label="Pulse" name="pulse" value={apptData.pulse}/>
-        <FormInput label="Blood Pressure" name="bp" value={apptData.bp}/>
-      </form>
-
-      <div className="flex">
-        <div>
-          <h2>Appointment Details</h2>
-          <ul className="list-disc px-4">
-            <li>Doctor Name</li>
-            <li>Time</li>
-            <li>Annual Exam</li>
-            <ul className="list-disc px-4">
-              <li>Breast Exam</li>
-              <li>Pelvic Exam</li>
-              <li>Pap Smear & STD Testing</li>
+        <form className="w-full space-y-2">
+          <FormInput label="Height" name="height" value={apptData.height} />
+          <FormInput label="Weight" name="weight" value={apptData.weight} />
+          <FormInput
+            label="Respiration Rate"
+            name="respRate"
+            value={apptData.respRate}
+          />
+          <FormInput label="Pulse" name="pulse" value={apptData.pulse} />
+          <FormInput label="Blood Pressure" name="bp" value={apptData.bp} />
+        </form>
+        <div className="flex justify-between gap-6">
+          <div>
+            <h2>Appointment Details</h2>
+            <ul className="list-disc pl-5">
+              <li>Doctor Name</li>
+              <li>Time</li>
+              <li>Annual Exam</li>
+              <ul className="list-disc pl-5">
+                <li>Breast Exam</li>
+                <li>Pelvic Exam</li>
+                <li>Pap Smear & STD Testing</li>
+              </ul>
             </ul>
-          </ul>
-        </div>
-        <div>
-          <h2>Additional Issues to Address</h2>
-          <ul className="list-disc px-4">
-            <li> Painful Periods </li>
-            <li>Click to add more... </li>
-          </ul>
-
-          <button className="border bg-red-400 p-4"> Assign Room </button>
+          </div>
+          <div className="flex grow flex-col justify-start">
+            <h2>Additional Issues to Address</h2>
+            <div className="flex h-full flex-col justify-between">
+              <ul className="list-disc pl-5">
+                <li> Painful Periods </li>
+                <li>Click to add more... </li>
+              </ul>
+              <button className="bg-contessa-500 border p-4 font-semibold text-white">
+                Assign Room
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </main>
@@ -69,11 +80,18 @@ const CheckIn = () => {
 
 function FormInput({ label, name, value }) {
   return (
-    <div className="flex">
-      <label className="w-1/2" htmlFor="height">
+    <div className="flex justify-around">
+      <label className="w-1/2 font-semibold" htmlFor="height">
         {label}
       </label>
-      <input type="text" className="border border-black bg-blue-300/50 text-red-700" id={name} name={name} readOnly value={value} />
+      <input
+        type="text"
+        className="border border-black px-3 py-1"
+        id={name}
+        name={name}
+        readOnly
+        value={value}
+      />
     </div>
   );
 }

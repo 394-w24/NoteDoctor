4ed7c147d9e13d3bf07b8473ae1cc14b17bfb697
PlsBeHumble <97078554+Geo-Li@users.noreply.github.com>
2024-02-20 21:23:34 -0600

    Turn input boxes into a function
    1. Adjusted the layout of the page and the input box
    2. Turn the input box div parts into reusable function

--- src/pages/CheckIn.jsx
+++ src/pages/CheckIn.jsx
@@ -9,58 +9,34 @@ const CheckIn = () => {
     queryFn: () => getAppt("KMa3tIVTBbLKpv9etNau"),
   });
 
-
-
   if (isLoading) return "Loading...";
 
-  
-
   return (
-    <main>
+    <main className="mx-auto my-4 max-w-md">
       <h1 className="font-cursive text-5xl">
-        {apptData.patient.firstName} {apptData.patient.lastName} ({apptData.patient.pronouns})
+        {apptData.patient.firstName} {apptData.patient.lastName} (
+        {apptData.patient.pronouns})
       </h1>
 
       <div className="flex justify-between">
         <div className="flex flex-col">
-          <p>{apptData.patient.dob.toDate().toLocaleDateString()}</p> 
+          <p>{apptData.patient.dob.toDate().toLocaleDateString()}</p>
           <p>{apptData.patient.age}</p>
         </div>
 
         <img
-                src="/nurse.webp"
-                alt="Maria Alvarez, RN"
-                className="aspect-square h-32 w-min rounded-full"
-              />
+          src="/nurse.webp"
+          alt="Maria Alvarez, RN"
+          className="aspect-square h-32 w-min rounded-full"
+        />
       </div>
 
-      <form>
-        <div className="flex">
-          <label htmlFor="height">Height</label>
-          <input type="text" id="height" name="name" readOnly value={apptData.height}/>
-        </div>
-
-        <div className="flex">
-          <label htmlFor="weight">Weight</label>
-          <input type="text" id="weight" name="name" readOnly value={apptData.weight}/>
-        </div>
-
-        <div className="flex">
-          <label htmlFor="respRate">Respiration Rate</label>
-          <input type="text" id="respRate" name="name" readOnly value={apptData.respRate}/>
-        </div>
-
-        <div className="flex">
-          <label htmlFor="pulse">Pulse</label>
-          <input type="text" id="pulse" name="name" readOnly value={apptData.pulse}/>
-        </div>
-
-        <div className="flex">
-          <label htmlFor="bp">Blood Pressure</label>
-          <input type="text" id="bp" name="name" readOnly value={apptData.bp}/>
-        </div>
-
-
+      <form className="w-full space-y-2">
+        <FormInput label="Height" name="height" value={apptData.height}/>
+        <FormInput label="Weight" name="weight" value={apptData.weight}/>
+        <FormInput label="Respiration Rate" name="respRate" value={apptData.respRate}/>
+        <FormInput label="Pulse" name="pulse" value={apptData.pulse}/>
+        <FormInput label="Blood Pressure" name="bp" value={apptData.bp}/>
       </form>
 
       <div className="flex">
@@ -76,26 +52,30 @@ const CheckIn = () => {
               <li>Pap Smear & STD Testing</li>
             </ul>
           </ul>
-
         </div>
         <div>
-        <h2 >
-          Additional Issues to Address
-        </h2>
-        <ul className="list-disc px-4">
-          <li> Painful Periods </li>
-          <li>Click to add more... </li>
-        </ul>
+          <h2>Additional Issues to Address</h2>
+          <ul className="list-disc px-4">
+            <li> Painful Periods </li>
+            <li>Click to add more... </li>
+          </ul>
 
-        <button className="p-4 border bg-red-400"> Assign Room </button>
-      
+          <button className="border bg-red-400 p-4"> Assign Room </button>
         </div>
-
       </div>
-
-
     </main>
   );
 };
+
+function FormInput({ label, name, value }) {
+  return (
+    <div className="flex">
+      <label className="w-1/2" htmlFor="height">
+        {label}
+      </label>
+      <input type="text" className="border border-black bg-blue-300/50 text-red-700" id={name} name={name} readOnly value={value} />
+    </div>
+  );
+}
 
 export default CheckIn;

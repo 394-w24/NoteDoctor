a7979937062df10d007e034b87110f754c4d7877
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-02-20 18:40:11 -0600

    Checkin Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-import PatientWelcome from './pages/PatientWelcome';
-import { StaffBio } from './pages/StaffBio';
+import { Route, Routes } from "react-router-dom";
+import CheckIn from "./pages/CheckIn";
+import PatientWelcome from "./pages/PatientWelcome";
+import { StaffBio } from "./pages/StaffBio";
 
 function App() {
-	return (
-		<Routes>
-			<Route path='/' element={<PatientWelcome />} />
-			<Route path='staffbio' element={<StaffBio />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<PatientWelcome />} />
+      <Route path="staffbio" element={<StaffBio />} />
+      <Route path="checkin" element={<CheckIn />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/CheckIn.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CheckIn = () => {
+  return (
+    <main>
+      <h1 className="font-cursive text-5xl">Carly Lowell (She/Her)</h1>
+    </main>
+  );
+};
+
+export default CheckIn;
